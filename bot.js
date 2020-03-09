38ab48ca0d8ab0baeb74026fe36995d5f80c9bab
GamingFR91 <52374013+GamingFR91@users.noreply.github.com>
2020-03-09 21:10:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,47 +1,29 @@
 const Discord = require("discord.js"); // We Call The Packages.
 // const PREFIX = "<"; // You can change this Prefix to whatever you want.
 const PREFIX = process.env.PREFIX;
-const Music = require('discord.js-musicbot-addon');
-var bot = new Discord.Client();
-const YT_key = process.env.YT_key;
-// Events.
-bot.on("ready", function() { 
-    let rotate = 0;
-            setInterval(function(){
-    
-                if(rotate === 0){
-                    client.user.setActivity('Nenorium | Network MCPE', {type: "STREAMING", url: 'https://www.twitch.tv/discordapp'});
-                    rotate = 1;       
-                }
-    
-                else if(rotate === 1){
-                    client.user.setActivity('nenorium.tk | 💎', {type: "STREAMING", url: 'https://www.twitch.tv/discordapp'});
-                    rotate = 2;
-                }
-    
-                else if(rotate === 2){
-                    client.user.setActivity(message.guild.memberCount + " utilisateurs sur notre discord !", {type: "STREAMING", url: 'https://www.twitch.tv/discordapp'});
-                    rotate = 0;
-                    
-                }
-    
-            }, 10 * 1000);
-        
-    
-});
-    
-    
-        
-const guild = bot.guilds.get('616221793467891722');
+const Query = require("minecraft-query");
 
+
+var bot = new Discord.Client();
+
+// Events.
+bot.on("ready", function() {
+    bot.user.setActivity('d!help | Draconium', { type: 'STREAMING' });
+    console.log(`${bot.user.username} est Prêt!`);
+});
 
 bot.on('guildMemberAdd', member => {
-    member.guild.channels.get('616221793467891724').send('**' + member.user.username + '**, a rejoint le serveur'); 
+
+    member.guild.channels.get('652994649497010176').send('Hey **' + member.user.tag + '**, bienvenue sur le meilleur le plus chili serveur  ---> **D R A C O N I U M** amuse toi sur ce serveur 0 insulte 100% chili le meilleur moyen de ce faire des potes :tada::hugging: !'); 
+
 });
 
 bot.on('guildMemberRemove', member => {
-    member.guild.channels.get('616221793467891724').send('**' + member.user.username + '**, a quitté le serveur');
+
+    member.guild.channels.get('652995719703166995').send('**' + member.user.tag + '** vien juste de quitter le serveur nous espérons te revoir très prochainement :slight_frown:');
+
     //
+
 });
 
 bot.on("message", function(message) {
@@ -56,124 +38,81 @@ bot.on("message", function(message) {
     var command = args[0].toLowerCase();
 
 // Commands.
-    
-    if(command === "members") {
-let guild = message.guild
-
-        let channel = message.channel
-
-        let guildicon = guild.icon_url
-
-        let members = guild.memberCount
-
-        let bots = guild.members.filter(m => m.user.bot).size
-
-        let humans = members - bots
-
-        let allchannels = guild.channels.size
-
-        let textchannels = guild.channels.filter(e => e.type === "text")
-
-        let voicechannels = guild.channels.filter(e => e.type === "voice")
-
-          var membercount = new Discord.RichEmbed()
-
-          .setColor(0xFF0011)
-
-          .setTitle(`Informations du Serveur`)
-
-          .setDescription(`Nom du Serveur: ${guild.name}`)
-
-          .addField("Propriétaire:", `${guild.owner}`, true)
-
-          .addField("ID du Serveur:", `${guild.id}`, true)
-
-          .addField("Région:", `${guild.region}`, true)
-
-          .addField("Niveau de Vérification:", `${guild.verificationLevel}`, true)
-
-          .addField("Nombre de Chaînes Vocaux:", `${voicechannels.size}`, true)
-
-          .addField("Nombre de Chaînes Textuels:", `${textchannels.size}`, true)
-
-          .addField("Nombre de Membres:", `${members}`, true)
-
-          .addField("Nombre de Bots:", `${bots}`, true)
-
-          .addField("Nombre d'Humains:", `${humans}`, true)
-
-          .addField("Nombre de Rôles:", `${guild.roles.size}`, true)
-
-          .addField(`Nombre d'emojis: (${guild.emojis.size})`, `- ${guild.emojis.array()}`, true);
-
-          .setFooter(`Date de création: ${guild.createdAt}`)
-
- 
-          
-message.channel.send('membercount');
-
-};
-    
     if (command == "help") {
         var embedhelpmember = new Discord.RichEmbed()
             .setAuthor("💬 Liste des Commandes.")
-            .addField(" - avatar", "Affiche ton avatar(non fonctionnel pour l'instant).")
+            .addField(" - avatar", "Affiche ton avatar.")
             .addField(" - ping", "PING PONG.")
             .addField(" - ip", "Affiche l'IP et le Port du serveur.")
+            .addField(" - report", "Permettre de Signaler quelqu'un vers le staff.")
+            .addField(" - suggest", "Permet d'envoyer une Suggestion au Staff")
             .setColor(0xFF0011)
-            .setFooter("Ⓒ 2019 Arthania.", bot.user.displayAvatarURL);
+            .setFooter("Ⓒ 2019-2020 Draconium.", bot.user.displayAvatarURL);
         var embedhelpadmin = new Discord.RichEmbed()
             .setAuthor("💬 Commandes de Modération.")
             .addField(" - clear", "Clear jusqu'à **99** Messages.")
             .addField(" - kick", "Expulser un membre du serveur.")
             .setColor(0xFF0011)
-            .setFooter("Ⓒ 2019 Arthania.", bot.user.displayAvatarURL);
-            message.channel.send(embedhelpmember);
-            message.channel.send(embedhelpadmin);
+            .setFooter("Ⓒ 2019-2020 Draconium.", bot.user.displayAvatarURL);
+            message.author.send(embedhelpmember);
+            message.author.send(embedhelpadmin);
     };
    
+
+    
     if (command == "ip") {
+
         var embedserverip = new Discord.RichEmbed()
 
-            .setAuthor(">>Arthania<<")
+            .setAuthor("Draconium")
 
             .addField("📡IP:", "Soon...")
 
             .addField("🛰️Port:", "Soon...")
         
-            .addField("<:mcpe:657609657216401408>Minecraft:", "1.14.0")
+            .addField("<:MCPE_Logo:652637305999458354>Minecraft:", "1.14")
         
-            .addField("🔧Statut:", "En Maintenance")
-        
-            .addField("🌴Vote:", "Soon...")
+            .addField("<:online:675622232155881495>Status:", "En Développement")
 
             .setColor(0xFF0011)
 
-            .setFooter("Ⓒ 2019 Arthania.", bot.user.displayAvatarURL);
+            .setFooter("Ⓒ 2019-2020 Draconium.", bot.user.displayAvatarURL);
 
         
             message.channel.send(embedserverip);
-
+            message.delete();
         
 
     };
 
     if (command == "avatar") {
-        if(!args[1]) return(args[1] === "<@${message.autor.id}>");
         let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         message.channel.send({
                embed: {
-                  title: `Avatar de ${member.user.username} `,
+                  title: `Photo de profil de ${member.user.username}:`,
                   image: {
                       url: member.user.displayAvatarURL
                   },
                   color: 0xFF0011
                }
         })
+        message.delete();
     };
 
     if (command == "ping") {
-        message.channel.send("**:ping_pong: PONG!**");
+        let PingEmbed = new Discord.RichEmbed()
+        .setAuthor("♥️Ping")
+        .addField("🏓Pong", "```" + bot.ping + "ms```")
+        .setColor(0xFF0011);
+        message.delete();
+        message.channel.send(PingEmbed);
+    };
+    
+    if (command == "test") {
+        const DiscordServ = bot.guilds.get(message.guild.id)
+        message.channel.send(DiscordServ.memberCount);
+        message.channel.send(DiscordServ.owner.user.tag);
+        message.channel.send(bot.ping + "ping");
     };
 
     if(command === "clear") {
@@ -185,13 +124,14 @@ message.channel.send('membercount');
         message.channel.fetchMessages({limit: messagesToDelete})
         .then(messages => message.channel.bulkDelete(messages.size + 1))
         .catch(error => message.channel.send(`❌ Désolé ${message.author}, Échec du Clear car: *${error}*.`));
+        message.delete();
     };
 
     if(command == "kick") {
         message.delete()
         let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         if(!kUser) return message.channel.send("❌ Merci de **@mention** la personne à Expulser!");
-        let kReason = args.join(" ").slice(0);
+        let kReason = args.slice(2).join(" ");
         if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("**🔒 Désolé, tu ne peux pas faire ça.**");
         if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("❌ Échec du Kick, j'ai besoin d'un rôle plus haut.");
     
@@ -207,6 +147,80 @@ message.channel.send('membercount');
         message.guild.member(kUser).kick(kReason);
         adminlog.send(kickEmbed);
     };
+    if(command == "report") {
+        message.delete()
+        let Report = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!Report) return message.channel.send("❌ Merci de **@mention** la personne à Signaler!");
+        let rReason = args.slice(2).join(" ");
+        if(!rReason) return message.channel.send("❌ Merci de donner la Raison de votre Report!");
+        
+        
+        let ReportEmbed = new Discord.RichEmbed()
+        .setDescription("**Report**")
+        .setColor(0xFF0011)
+        .addField("Personne", `${Report}`)
+        .addField("Report par", `<@${message.author.id}>`)
+        .addField("Raison", `**\`\`\`${rReason}\`\`\`**`);
+    
+        let reportlog = message.guild.channels.find(`name`, "【🚫】report");
+        if(!reportlog) return message.channel.send("❌ Désolé, j'ai besoin de me connecter dans un channel de Report.");
+
+
+        reportlog.send(ReportEmbed).then(m => {
+         m.react('✅');
+         m.react('❌');
+        })
+        
+
+    };
+    
+    if(command == "suggest") {
+
+        message.delete()
+
+
+
+        let Suggestion = args.slice(1).join(" ");
+
+        if(!Suggestion) return message.channel.send("❌ Merci de donner votre Suggestion!");
+
+        
+
+        
+
+        let SuggEmbed = new Discord.RichEmbed()
+
+        .setDescription("**Suggestion**")
+
+        .setColor(0xFF0011)
+
+
+       
+
+
+        .addField("Suggestion de: ", `<@${message.author.id}>`)
+       
+        
+        
+       
+
+        .addField("Idée", `**\`\`\`${Suggestion}\`\`\`**`);
+
+    
+
+        let sugglog = message.guild.channels.find(`name`, "【🔧】suggestions");
+
+        if(!sugglog) return message.channel.send("❌ Désolé, j'ai besoin de me connecter dans un channel de Suggestion.");
+
+        sugglog.send(SuggEmbed).then(s => {
+
+         s.react('✅');
+
+         s.react('❌');
+
+        })
+        
+       };
 
 });
 
